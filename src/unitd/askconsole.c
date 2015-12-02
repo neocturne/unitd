e692cc7a655b016b15b6f83fa8069c1b0d6b5220
Matthias Schiffer <mschiffer@universe-factory.net>
2015-12-02 10:50:28 +0100

    askconsole: fix copyright header

--- src/unitd/askconsole.c
+++ src/unitd/askconsole.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2015 Matthias Schiffer <mschiffer@universe-factory.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 2.1
